44fd1ebe6c7de8aee09696b2c988cafc9577a7ca
lukasmoser <moserlukas051194@gmail.com>
2016-12-21 19:50:29 +0100

    change ordering of method and add comments

--- submission/cross_validation.py
+++ submission/cross_validation.py
@@ -5,8 +5,95 @@ from mf_ccd import *
 from mf_sgd import *
 from ensemble_als_userfilter import *
 
+
+
+def split_data_k_numpy(ratings, k_indices_set, k):
+    """split the data and return numpy array of train and test"""
+    
+    K, index_split = k_indices_set.shape
+    index_tr = k_indices_set[np.where(np.arange(K) != (k-1))].reshape(((K-1)*index_split,1))[:,0]
+    index_te = k_indices_set[k-1]
+    
+    row = ratings.shape[0]
+    col = ratings.shape[1]
+    num = row*col
+    
+    rat_arr = ratings.toarray()
+    
+    # reshaping
+    valid_array_te = np.copy(rat_arr).reshape((num,1))
+    valid_array_tr = np.copy(rat_arr).reshape((num,1))  
+    
+    # create split
+    train = valid_array_tr
+    train[index_te] = 0
+    train = train.reshape((row,col))
+    
+    test = valid_array_te
+    test[index_tr] = 0
+    test = test.reshape((row,col))
+    
+    # ***************************************************
+    
+    return train, test
+
+
+
+def k_indices_set_generator(ratings, k=5, seed=48):
+    """return K different set of indices from that whole dataset, used for cross_validation""" 
+    # ***************************************************
+    # set seed
+    np.random.seed(seed)
+    
+    # generate random indices
+    row = ratings.shape[0]
+    col = ratings.shape[1]
+    num = row*col
+    
+    indices = np.random.permutation(num)
+    
+    index_split = int(num/k)
+    k_indices_set = np.zeros((k,index_split),dtype=np.int)   
+    
+    for i in range(k):    
+        id_start = index_split*i
+        id_end = index_split*(i+1)
+        k_indices_set[i] = indices[id_start:id_end]
+    # ***************************************************
+    return k_indices_set
+
+def split_data_k(ratings, k_indices_set, k):
+    """split the data according to k_indices_set ( containing K different set of indices)"""
+    # ***************************************************
+    K, index_split = k_indices_set.shape
+    index_tr = k_indices_set[np.where(np.arange(K) != (k-1))].reshape(((K-1)*index_split,1))[:,0]
+    index_te = k_indices_set[k-1]
+    
+    row = ratings.shape[0]
+    col = ratings.shape[1]
+    num = row*col
+    
+    rat_arr = ratings.toarray()
+    
+    # reshaping
+    valid_array_te = np.copy(rat_arr).reshape((num,1))
+    valid_array_tr = np.copy(rat_arr).reshape((num,1))  
+    
+    # create split
+    train = valid_array_tr
+    train[index_te] = 0
+    train = train.reshape((row,col))
+    
+    test = valid_array_te
+    test[index_tr] = 0
+    test = test.reshape((row,col))
+    
+    # ***************************************************
+    return sp.lil_matrix(train), sp.lil_matrix(test)
+
 def cross_validation_run(ratings, method, K, num_features=5, lambda_user=0.01, lambda_item=0.01, gamma = 0.01, max_iter=10,
                          lambda_ridge=0.01, weight_als=0.75, num_user_neighbours=50, nosubmit=1):
+    """run a cross validation algorithm, return errors, but also best train/test for the function requested"""
     
     k_indices_set = k_indices_set_generator(ratings,K)    
 
@@ -48,86 +135,6 @@ def cross_validation_run(ratings, method, K, num_features=5, lambda_user=0.01, l
             train = train_cross
             test = test_cross
         
-    return train_rmse_arr, validation_rmse_arr, train, test, tr_min, vr_min, us_ft_min, itm_ft_min, 
+    return train_rmse_arr, validation_rmse_arr, train, test, tr_min, vr_min, us_ft_min, itm_ft_min
 
-def split_data_k(ratings, k_indices_set, k):
-    
-    K, index_split = k_indices_set.shape
-    index_tr = k_indices_set[np.where(np.arange(K) != (k-1))].reshape(((K-1)*index_split,1))[:,0]
-    index_te = k_indices_set[k-1]
-    
-    row = ratings.shape[0]
-    col = ratings.shape[1]
-    num = row*col
-    
-    rat_arr = ratings.toarray()
-    
-    # reshaping
-    valid_array_te = np.copy(rat_arr).reshape((num,1))
-    valid_array_tr = np.copy(rat_arr).reshape((num,1))  
-    
-    # create split
-    train = valid_array_tr
-    train[index_te] = 0
-    train = train.reshape((row,col))
-    
-    test = valid_array_te
-    test[index_tr] = 0
-    test = test.reshape((row,col))
-    
-    # ***************************************************
-    
-    #print (valid_ratings, train, test)
-    return sp.lil_matrix(train), sp.lil_matrix(test)
 
-def k_indices_set_generator(ratings, k=5, seed=48):
-    
-    # set seed
-    np.random.seed(seed)
-    
-    # generate random indices
-    row = ratings.shape[0]
-    col = ratings.shape[1]
-    num = row*col
-    
-    indices = np.random.permutation(num)
-    
-    index_split = int(num/k)
-    k_indices_set = np.zeros((k,index_split),dtype=np.int)   
-    
-    for i in range(k):    
-        id_start = index_split*i
-        id_end = index_split*(i+1)
-        k_indices_set[i] = indices[id_start:id_end]
-    
-    return k_indices_set
-
-def split_data_k_numpy(ratings, k_indices_set, k):
-    
-    K, index_split = k_indices_set.shape
-    index_tr = k_indices_set[np.where(np.arange(K) != (k-1))].reshape(((K-1)*index_split,1))[:,0]
-    index_te = k_indices_set[k-1]
-    
-    row = ratings.shape[0]
-    col = ratings.shape[1]
-    num = row*col
-    
-    rat_arr = ratings.toarray()
-    
-    # reshaping
-    valid_array_te = np.copy(rat_arr).reshape((num,1))
-    valid_array_tr = np.copy(rat_arr).reshape((num,1))  
-    
-    # create split
-    train = valid_array_tr
-    train[index_te] = 0
-    train = train.reshape((row,col))
-    
-    test = valid_array_te
-    test[index_tr] = 0
-    test = test.reshape((row,col))
-    
-    # ***************************************************
-    
-    #print (valid_ratings, train, test)
-    return train, test
